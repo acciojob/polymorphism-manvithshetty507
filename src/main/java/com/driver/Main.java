34cc8e59dc503849edbf59b29625a4d685838df9
manvithshetty507 <manvithshetty18@gmail.com>
2023-02-08 15:27:16 +0530

    first commit

--- src/main/java/com/driver/Main.java
+++ src/main/java/com/driver/Main.java
@@ -2,4 +2,39 @@ package com.driver;
 
 public class Main {
 
+    static class Product{
+        int x;
+        int y;
+        int z;
+        public int product(int x,int y){
+            this.x = x;
+            this.y = y;
+
+            return x;
+        }
+        public int product(int x,int y,int z){
+            this.x = x;
+            this.y = y;
+            this.z = z;
+
+            return z;
+        }
+        public int product(double x,double y){
+            this.x = (int)x;
+            this.y = (int)y;
+
+            return (int)y;
+        }
+    }
+
+    public static void main(String[] args) {
+
+        Product p = new Product();
+
+        p.product(10,9);
+        p.product(3,4,5);
+        p.product(8.8,9.9);
+
+    }
+
 }
